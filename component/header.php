f12566d1313b9e48256925e958be5c35b4ff9292
Htet Nadi <htetnadi82@gmail.com>
2024-05-07 18:33:41 +0630

    index view

--- component/header.php
+++ component/header.php
@@ -40,16 +40,16 @@
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Blog</a>
+                        <a class="nav-link" href="index.php">Blog</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Login</a>
+                        <a class="nav-link" href="login.php">Login</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Register</a>
+                        <a class="nav-link" href="register.php">Register</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Logout</a>
+                        <a class="nav-link" href="logout.php">Logout</a>
                     </li>
                 </ul>
                 <form class="d-flex" role="search">
--- index.php
+++ index.php
@@ -1,7 +1,38 @@
-<?php include('component/header.php') ?>
-
-    <h1>Index Page</h1>
-
-<?php include('component/footer.php') ?>
-
+<?php
+include "db.php";
+$query = "SELECT * FROM articles";
+$result = mysqli_query($con, $query);
+if ($result) {
+    $articles = $result;
     
+} else {
+    echo mysqli_connect_error();
+}
+
+?>
+
+<?php include "component/header.php" ?>
+
+<div class="row row-cols-1 row-cols-md-2 row-cols-lg-3">
+    <?php while($article = mysqli_fetch_assoc($articles)): ?>
+        <div class="col">
+            <div class="card" style="width: 18rem;">
+                <img src="<?= $article['image'] ?>" class="card-img-top object-fit-cover" alt="img" style="height: 10rem">
+                <div class="card-body">
+                    <a href="blog-view.php?id=<?= $article['id'] ?>" class="h3"><?= htmlspecialchars_decode($article['title']) ?></a>
+                    <p class="card-text"><?= htmlspecialchars_decode(substr($article['about'], 0, 100)) ?>
+                    </p>
+
+                    <?php 
+                        $str_to_array = explode(" ", htmlspecialchars_decode($article['about']));
+                        $slice_array = array_slice($str_to_array,0,15);
+                        $result = implode(" ", $slice_array);
+                        $result .= " ... ";
+                        print_r($result);
+                    ?>
+                </div>
+            </div>
+        </div>
+    <?php endwhile; ?>
+</div>
+<?php include  "component/footer.php" ?>
